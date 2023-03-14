ca46b7d70f1ba9306c81dc8c7a4c235b1888ef08
Gwabstech <gwabstech@gmail.com>
2023-03-14 22:03:49 +0100

    step 1 done

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-
 const port = 3000;
+app.use(express.json());
 
-app.get('/home', (req, res) => {
+// reading the tours from memory
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     // sending json
-    .json({ message: `Hello from ${port}`, status: 200, name: 'Abubakar' });
+    .json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'error',
+          message: 'Error writing to file',
+        });
+      } else {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tours: newTour,
+          },
+        });
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
