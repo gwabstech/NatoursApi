b5889bb740bc2d52d23f0159fcb1aa5623e6d067
Gwabstech <gwabstech@gmail.com>
2023-03-15 09:17:47 +0100

    working on routes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const fs = require('fs');
+const { get } = require('http');
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
-// reading the tours from memory
+// reading the tours from memory and return a list of tours
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// function to get tour by id
+function getTourById(id) {
+  return tours.find((tour) => tour.id === parseInt(id));
+}
 app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
@@ -18,6 +24,28 @@ app.get('/api/v1/tours', (req, res) => {
       result: tours.length,
       data: {
         tours: tours,
+      },
+    });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params.id);
+
+  if (req.params.id > tours.length) {
+    return res
+      .status(404)
+      .json({
+        status: 'Not found',
+        message: `Tour with id ${req.params.id} does not exist`,
+      });
+  }
+  res
+    .status(201)
+    // sending json
+    .json({
+      status: 'success',
+      data: {
+        tour: getTourById(req.params.id),
       },
     });
 });
