bcf74e467cb2699594c13f762d18c4f0951ed86e
Gwabstech <gwabstech@gmail.com>
2023-04-23 22:22:25 +0100

    middleware

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const { get } = require('http');
 
 const app = express();
 const port = 3000;
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  // console.log('Hello, world!');
+  (req.requestTime = new Date()).toISOString();
+  next();
+});
 
 // reading the tours from memory and return a list of tours
 const tours = JSON.parse(
@@ -12,32 +19,58 @@ const tours = JSON.parse(
 );
 
 // function to get tour by id
-function getTourById(id) {
+function getTour(id) {
   return tours.find((tour) => tour.id === parseInt(id));
 }
-app.get('/api/v1/tours', (req, res) => {
-  res
-    .status(200)
-    // sending json
-    .json({
-      status: 'success',
-      result: tours.length,
-      data: {
-        tours: tours,
-      },
-    });
-});
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const deleteTourById = (req, res) => {
   console.log(req.params.id);
 
   if (req.params.id > tours.length) {
-    return res
-      .status(404)
-      .json({
-        status: 'Not found',
-        message: `Tour with id ${req.params.id} does not exist`,
-      });
+    return res.status(404).json({
+      status: 'failed',
+      data: {
+        tour: `invalid tour id ${req.params.id}`,
+      },
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+const updateTour = (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      data: {
+        tour: `invalid tour id ${req.params.id}`,
+      },
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+const getTourById = (req, res) => {
+  //console.log(req.params.id);
+
+  const id = req.params.id;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'Not found',
+      date: req.requestTime,
+      message: `Tour with id ${id} does not exist`,
+    });
   }
   res
     .status(201)
@@ -45,12 +78,12 @@ app.get('/api/v1/tours/:id', (req, res) => {
     .json({
       status: 'success',
       data: {
-        tour: getTourById(req.params.id),
+        tour: getTour(req.params.id),
       },
     });
-});
-
-app.post('/api/v1/tours', (req, res) => {
+};
+app.delete('/api/v1/tour/:id', deleteTourById);
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -75,7 +108,27 @@ app.post('/api/v1/tours', (req, res) => {
       }
     }
   );
-});
+};
+
+const getAllTours = (req, res) => {
+  res
+    .status(200)
+    // sending json
+    .json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+};
+
+app
+  .route('/api/v1/tour/:id')
+  .get(getTourById)
+  .patch(updateTour)
+  .delete(deleteTourById);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}..`);
